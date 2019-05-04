5210f45f5ea7e656365c572d9db230dd9513da3a
Pieter Wuille <pieter.wuille@gmail.com>
2019-05-03 19:08:00 -0700

    No CHash256 class

--- test.cpp
+++ test.cpp
@@ -1,12 +1,10 @@
 void Finalizer(unsigned char* hash);
 void SHA256D64(unsigned char* output, const unsigned char* input, unsigned int blocks);
 
-struct CHash256 {
-    void Finalize(unsigned char* hash) {
-        unsigned char buf[32];
-        Finalizer(buf);
-    }
-};
+void Finalize(unsigned char* hash) {
+    unsigned char buf[32];
+    Finalizer(buf);
+}
 
 int main() {
     for (int i = 0; i <= 32; ++i) {
@@ -16,7 +14,7 @@ int main() {
             in[j] = (unsigned char)(j+192);
         }
         for (int j = 0; j < i; ++j) {
-            CHash256().Finalize(out1 + 32 * j);
+            Finalize(out1 + 32 * j);
         }
         SHA256D64(out2, in, i);
     }
