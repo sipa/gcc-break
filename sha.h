fdf42e50f2b75b66c47749adc8569aaa0b61afd7
Pieter Wuille <pieter.wuille@gmail.com>
2019-05-03 18:49:30 -0700

    Simplify

--- sha.h
+++ sha.h
@@ -4,14 +4,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-class CSHA256
+struct CSHA256
 {
-private:
-    uint32_t s[8];
-    unsigned char buf[64];
-    uint64_t bytes;
-
-public:
     static const size_t OUTPUT_SIZE = 32;
     CSHA256();
     CSHA256& Write(const unsigned char* data, size_t len);
